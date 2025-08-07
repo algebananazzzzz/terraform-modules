a41a50e078eab789d8cd356881f1e27d99f1917d
algebananazzzzz <algebananazzzzz.devops@gmail.com>
2025-08-07 18:03:46 +0800

    fix(cloudfront s3 origin): correct bucket reference in origin access control

--- cloudfront_s3_origin/oac.tf
+++ cloudfront_s3_origin/oac.tf
@@ -1,8 +1,8 @@
 resource "aws_cloudfront_origin_access_control" "origin_access_control" {
   name = coalesce(
     var.origin_access_control_name,
-    local.create_new_bucket ? aws_s3_bucket.new[0].bucket_regional_domain_name : null,
-    try(var.origin_bucket_use_existing.bucket_regional_domain_name, "S3Origin")
+    local.create_new_bucket ? aws_s3_bucket.new[0].bucket : null,
+    try(var.origin_bucket_use_existing.bucket, "S3Origin")
   )
   description                       = var.origin_access_control_description
   origin_access_control_origin_type = "s3"
